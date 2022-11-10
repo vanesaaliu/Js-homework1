9c66115ace5c36fcd3c235547b14aa58bd0a3f96
vanesaaliu <94717763+vanesaaliu@users.noreply.github.com>
2022-11-10 20:01:50 +0100

    detyra

---
+++ main.js
@@ -0,0 +1,62 @@
+
+//                                  HOMEWORK
+
+//Request a user’s name and display the response “Hello, [name]”.
+// let name=prompt('Type your name: ');
+// console.log('Hello '+ name);
+
+
+//Request a user’s year of birth, count his age, and display the result. 
+//Store the current year as a constant.
+// let birthYear=prompt('Year of birth: ')*1;
+// const currentYear=2022;
+// console.log(currentYear-birthYear);
+
+
+
+// Request a length of a side of a square from a user and display the perimeter of the square.
+// let sideLength=prompt('Side length: ')*1;
+// console.log(sideLength*4);
+
+
+
+//Request a radius of a circle and display the area of such a circle.
+// let rad=prompt('Write the radius of a circle: ')*1;
+// const pi=3.14;
+// console.log("Area of the circle is: "+(pi*rad*rad));
+
+
+// Request a distance in km between the two cities from a user, and the time they want to cover it within.
+// Count the speed needed to be on time.
+// let distance=prompt('Distance between two cities in km: ') * 1;
+// let time=prompt('Time: ')*1;
+// console.log('Speed: '+(distance/time)+ "km/s");
+
+
+// Create a currency converter. A user types in USD and the program converts them to EUR.
+// The exchange rate should be stored as a constant.
+// let usd=prompt('Dollars: ')*1;
+// const exc=1.2;
+// console.log('The currency exchange is: '+(usd*exc)+" eur.");
+
+
+// A user states a flash drive memory in GB. 
+// The program has to count how many 820MB files can be stored on this flash drive.
+// let gb=prompt("Memory in GB:");
+// console.log("There can be stored:",((gb*1000)/820)," files");
+
+
+// A user types in an amount of money in a wallet and the price of one chocolate bar. 
+// The program counts how many chocolate bars the user can buy, and how much change will he/she have left.
+// let money=prompt("Money u got: ")*1;
+// let price=prompt("Price of a chocolate bar: ")*1;
+// let bar=money/price;
+// console.log("You can buy:",bar," and will have left :", money-(bar*price)," change");
+
+
+
+//The user types in a sum of bank deposits for 2 months with a yearly interest rate of 5%. 
+//The program counts the sum of interest.
+// let sum=prompt("Sum of bank deposites for two months: ")*1;
+// let interest=(sum/2)*0.5;
+// console.log("Sum of interest:",interest);
